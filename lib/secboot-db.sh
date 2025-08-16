576029d19dfdd88c119c6d5f3cc2f40f28ac02a2
dcx7c5 <dcxdevelopment@protonmail.com>
2025-08-16 22:26:45 +0200

    Expand `dystopian-secboot` database operations: add GUID management, enhance key/cert handling functions, and standardize error handling with `jq` and file operations.

--- lib/secboot-db.sh
+++ lib/secboot-db.sh
@@ -2,18 +2,113 @@
 # shellcheck disable=SC2001
 
 
-secboot_save_key() {
-    if ! jq -e --arg type "$1" --arg val "$2" '.sb[$type] = $val' "$DC_DB"; then
-        echoe "Failed adding Secure Boot $1: $2 to database"
-        return 1
-    fi
-    return 0
+set_owner_guid() {
+  jq -e --arg val "$1" '.GUID = $val' -- "$DS_DB" > "$DS_DB.tmp" || {
+    echoe "Failed setting GUID for $1"
+    rm -f -- "$DS_DB.tmp"
+    return 1
+  }
+
+  mv -- "${DS_DB}.tmp" "$DS_DB" || {
+    echoe "Failed moving $DS_DB"
+    rm -f -- "$DS_DB.tmp"
+    return 1
+  }
+
+  rm -f -- "${DS_DB}.tmp" || {
+    echoe "Removing $DS_DB.tmp failed"
+    rm -f -- "$DS_DB.tmp"
+    return 1
+  }
+  return 0
 }
 
-secboot_save_vendor() {
-    if ! jq -e --arg type "$1" --arg val "$2" '.sb.VENDOR[$type] = $val' "$DC_DB"; then
-        echoe "Failed adding Secure Boot Vendor $1: $2 to database"
-        return 1
-    fi
-    return 0
+get_owner_guid() {
+  jq -r '.GUID // empty' -- "$DS_DB" || return 1
+}
+
+set_key_guid() {
+  jq -e --arg idx "$1" \
+        --arg val "$2" \
+        '.[$idx].GUID = $val' -- "$DS_DB" > "$DS_DB.tmp" || {
+          echoe "Failed setting GUID for host $1"
+          rm -f -- "$DS_DB.tmp"
+          return 1
+        }
+
+  mv -- "${DS_DB}.tmp" "$DS_DB" || {
+    echoe "Failed moving $DS_DB"
+    rm -f -- "$DS_DB.tmp"
+    return 1
+  }
+
+  rm -f -- "${DS_DB}.tmp" || {
+    echoe "Removing $DS_DB.tmp failed"
+    rm -f -- "DS_DB.tmp"
+    return 1
+  }
+  return 0
+}
+
+get_key_guid() {
+  jq -r --arg idx "$1" '.[$idx].GUID // empty' -- "$DS_DB" || return 1
+}
+
+set_key_value() {
+  jq -e --arg idx "$1" \
+        --arg key "$2" \
+        --arg val "$3" \
+        '.[$idx].key[$key] = $val' -- "$DS_DB" > "$DS_DB.tmp" || {
+          echoe "Failed setting $2 = $3 for host $1"
+          rm -f -- "$DS_DB.tmp"
+          return 1
+        }
+
+  mv -- "${DS_DB}.tmp" "$DS_DB" || {
+    echoe "Failed moving $DS_DB"
+    rm -f -- "$DS_DB.tmp"
+    return 1
+  }
+
+  rm -f -- "${DC_DB}.tmp" || {
+    echoe "Removing $DC_DB.tmp failed"
+    rm -f -- "$DC_DB.tmp"
+    return 1
+  }
+  return 0
+}
+
+set_cert_value() {
+  jq -e --arg idx "$1" \
+        --arg key "$2" \
+        --arg val "$3" \
+        '.[$idx].cert[$key] = $val' -- "$DS_DB" > "$DS_DB.tmp" || {
+          rm -f -- "$DS_DB.tmp"
+          return 1
+        }
+
+  mv -- "${DS_DB}.tmp" "$DS_DB" || {
+    echoe "Failed moving $DS_DB"
+    rm -f -- "$DS_DB.tmp"
+    return 1
+  }
+
+  rm -f -- "${DS_DB}.tmp" || {
+    echoe "Removing $DS_DB.tmp failed"
+    rm -f -- "$DS_DB.tmp"
+    return 1
+  }
+  return 0
+}
+
+get_key_value() {
+  jq -e --arg idx "$1" \
+        --arg key "$2" \
+        '.[$idx].key[$key] // empty' -- "$DS_DB" || return 1
+}
+
+get_cert_value() {
+  jq -e --arg idx "$1" \
+        --arg key "$2" \
+        '.[$idx].cert[$key] // empty' -- "$DS_DB" || return 1
 }
