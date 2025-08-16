56ae398e108618f86ce4e6f56c7fd070f9b14afa
dcx7c5 <dcxdevelopment@protonmail.com>
2025-08-16 22:28:05 +0200

    Expand `dystopian-aurtool` functionality: - Implement `aurtool.sh` library for AUR-specific operations. - Update `aurtool-db.json` with initial values and replace `access_token` with `repo_token`. - Enhance `aurtool-db.sh` with functions for setting and retrieving GitHub tokens and package metadata using `jq`. - Ensure proper library sourcing in `dystopian-aurtool` script.

--- lib/aurtool-db.sh
+++ lib/aurtool-db.sh
@@ -1,3 +1,23 @@
 # shellcheck shell=sh
 # shellcheck disable=SC2001
 
+set_github_token() {
+  jq -e --arg val "$1" '.GITHUB_TOKEN = $val' -- "$DA_DB" || return 1
+}
+
+get_github_token() {
+  jq -r '.GITHUB_TOKEN // empty' -- "$DA_DB" || return 1
+}
+
+set_package_value() {
+  jq -e --arg idx "$1" \
+        --arg key "$2" \
+        --arg val "$3" \
+        '.packages[$idx][$key] = $val' -- "$DA_DB" || return 1
+}
+
+get_package_value() {
+  jq -r --arg idx "$1" \
+        --arg key "$2" \
+        '.packages[$idx][$key] // empty' -- "$DA_DB" || return 1
+}
---
+++ lib/aurtool.sh
@@ -0,0 +1,4 @@
+# shellcheck shell=sh
+# shellcheck disable=SC2001
+# shellcheck disable=SC2181
+
